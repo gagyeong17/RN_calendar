a8e71130ad0d4e1c32e0f17cda78af53bb27df6a
GYMMX <rkrud951117@naver.com>
2023-02-26 04:03:45 +0900

    level 2 까지 구현

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./Navigation";
 
 export default function App() {
   return (
     <>
       <Navigation />
-      {/* <Text>나와?</Text> */}
       <StatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#eee",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
